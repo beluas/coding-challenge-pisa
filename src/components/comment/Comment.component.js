ea5a870ad255fb3ace211db9914355b7bf7eb23c
Ivan Maddaluno <ivanmaddaluno1993@gmail.com>
2020-06-19 17:56:31 +0200

    style added to comment component

--- src/components/comment/Comment.component.js
+++ src/components/comment/Comment.component.js
@@ -4,8 +4,17 @@ import "./comment.styles.scss";
 const Comment = ({ name, body }) => {
 	return (
 		<div className="comment-box">
-			<h3>{name}</h3>
-			<p>{body}</p>
+			<div className="post-box">
+				<img
+					src="https://dummyimage.com/150X150/000/fff"
+					alt="Dummy Image"
+				/>
+				<div className="desc">
+					<h3>{name}</h3>
+					<p>{body}</p>
+				</div>
+			</div>
+
 			<hr />
 		</div>
 	);
